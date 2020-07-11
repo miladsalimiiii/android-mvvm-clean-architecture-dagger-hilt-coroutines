3e3e8b5e97189395ae38db54bf6cfa31040d7527
miladsi90 <miladsalimi69@yahoo.com>
2020-07-12 02:36:17 +0430

    Create item transaction layout for recyclerView adapter , closed #2

--- app/src/main/java/com/milad/diver/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/milad/diver/ui/setting/SettingFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.milad.diver.R
 
@@ -21,7 +20,7 @@ class SettingFragment : Fragment() {
     ): View? {
         settingViewModel =
                 ViewModelProviders.of(this).get(SettingViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val root = inflater.inflate(R.layout.fragment_setting, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
         return root
     }
--- app/src/main/java/com/milad/diver/ui/transactions/TransactionFragment.kt
+++ app/src/main/java/com/milad/diver/ui/transactions/TransactionFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.milad.diver.R
 
@@ -21,7 +20,7 @@ class TransactionFragment : Fragment() {
     ): View? {
         transactionViewModel =
                 ViewModelProviders.of(this).get(TransactionViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
+        val root = inflater.inflate(R.layout.fragment_transaction, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
 
         return root
